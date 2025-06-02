adaaa335d5e3a1194fd84d4a791b0f7207f2b638
NQKhanh296 <156611827+NQKhanh296@users.noreply.github.com>
2025-06-02 23:31:34 +0200

    wrong using deleted

--- Assets/Scripts/HealDrop.cs
+++ Assets/Scripts/HealDrop.cs
@@ -1,7 +1,6 @@
 using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class HealDrop : MonoBehaviour
